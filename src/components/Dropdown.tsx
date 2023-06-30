ef92f020847d1d42f88e3fb2bf37a180a364e490
Gabriel <gabriel.gomes_fernandes@hotmail.com>
2023-06-30 20:08:39 -0300

    mudança das fontes

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -106,7 +106,7 @@ function Dropdown() {
               }}
               className="font-lato block w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
             >
-              Lato
+              Noto
             </button>
           </li>
         </ul>
--- src/features/fontFamilyContext/fontFamiliSlice.ts
+++ src/features/fontFamilyContext/fontFamiliSlice.ts
@@ -2,7 +2,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  font: 'serif'
+  font: 'lato'
 };
 
 const fontFamilySlice = createSlice({
